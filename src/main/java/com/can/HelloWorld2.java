51ee83ce20d90a03043f242d4fc138d848dc209b
hemincan <1027871354@qq.com>
2017-10-17 16:43:50 +0800

    567

--- src/main/java/com/can/HelloWorld2.java
+++ src/main/java/com/can/HelloWorld2.java
@@ -5,6 +5,7 @@ public class HelloWorld2 {
 	System.err.println("啦啦啦啦啦啦");
 	System.err.println("冲突");
   System.err.println("冲突22222");
-
+System.err.println("冲突22222");
+System.err.println("冲突22222");
 }
 }
